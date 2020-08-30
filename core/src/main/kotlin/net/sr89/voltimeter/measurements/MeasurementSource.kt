8d4bda0e0d75090193c940be5a1f5df110093edb
Simone <s@sr89.net>
2020-08-31 00:12:38 +0200

    Nice sinusoidal voltage source

--- core/src/main/kotlin/net/sr89/voltimeter/measurements/MeasurementSource.kt
+++ core/src/main/kotlin/net/sr89/voltimeter/measurements/MeasurementSource.kt
@@ -1,12 +1,14 @@
 package net.sr89.voltimeter.measurements
 
 import com.badlogic.gdx.utils.TimeUtils
-import kotlin.random.Random
+import kotlin.math.sin
 
 class MeasurementSource {
-    private val random: Random = Random(2)
+    private var a: Double = 0.0
 
     fun nextMeasurement(): Measurement {
-        return Measurement(random.nextFloat() * 5, TimeUtils.millis())
+        a += 0.1
+        val voltage = sin(2 * a).toFloat() * 5
+        return Measurement(voltage, TimeUtils.millis())
     }
 }
