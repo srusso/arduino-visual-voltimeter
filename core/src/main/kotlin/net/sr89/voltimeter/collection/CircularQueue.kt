b3d82d2a2a3f07910e226e61791f4d92ab860858
Simone <s@sr89.net>
2020-11-03 21:46:52 +0100

    Added test for CircularQueue.kt

--- core/src/main/kotlin/net/sr89/voltimeter/collection/CircularQueue.kt
+++ core/src/main/kotlin/net/sr89/voltimeter/collection/CircularQueue.kt
@@ -2,11 +2,15 @@ package net.sr89.voltimeter.collection
 
 import com.badlogic.gdx.utils.Queue
 
+/**
+ * A FIFO data structure that keeps at most [maxSize] elements,
+ * pruning from the head (oldest).
+ */
 class CircularQueue<T>(private val maxSize: Int) {
     private val values: Queue<T> = Queue(maxSize)
 
     fun add(value: T) {
-        if (values.size >= maxSize - 1) {
+        if (values.size >= maxSize) {
             values.removeFirst()
         }
 
